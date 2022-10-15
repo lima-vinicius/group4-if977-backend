9ede6930ffe1ca15f04454b48faab59ee305da3d
valter-junior <valterjose.sjr@gmail.com>
2022-10-15 18:59:55 -0300

    Added auth verification to routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import student from '../controllers/user';
+import auth from '../middlewares/auth';
 
 router.post('/register', student.register);
 router.post('/login', student.login)
-router.get('/list-all', student.listAll)
-router.post('/list-student-by-id', student.list)
+router.get('/list-all', auth, student.listAll)
+router.post('/list-student-by-id', auth, student.list)
 
 export default router;
